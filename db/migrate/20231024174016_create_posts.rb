b9669bc7e9d8ad0aae3acb32120113c33b1fd92f
WHB901 <abdulwhbhussain@gmail.com>
2023-10-25 01:12:52 +0500

    Update readme file

--- db/migrate/20231024174016_create_posts.rb
+++ db/migrate/20231024174016_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.references :user, null: false, foreign_key: true, index: true
       t.string :title
-      t.string :text
+      t.text :text
       t.integer :comments_counter, default: 0
       t.integer :likes_counter, default: 0      
 
