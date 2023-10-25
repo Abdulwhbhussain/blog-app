4e95cc0cd98c2f98effcf36b5852dd01c5efe015
WHB901 <abdulwhbhussain@gmail.com>
2023-10-25 08:08:06 +0500

    Methods in Models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :increment_no_of_comments
+
+  private
+
+  def increment_no_of_comments
+    post.update(comments_counter: post.comments_counter + 1)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :increment_no_of_likes
+
+  private
+
+  def increment_no_of_likes
+    post.update(likes_counter: post.likes_counter + 1)
+  end
 end
