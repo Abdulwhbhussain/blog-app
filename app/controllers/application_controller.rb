5fff2b3e35d792ff2a93cd67eda0225020e6e12c
WHB901 <abdulwhbhussain@gmail.com>
2023-10-30 04:34:49 +0500

    Add current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  # current_user is a method that returns the currently logged in user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
