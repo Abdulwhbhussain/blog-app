341ce02ba33371fa65d4ae881f1e7e20a83dff45
WHB901 <abdulwhbhussain@gmail.com>
2023-10-30 12:56:06 +0500

    User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # current_user is a method that returns the currently logged in user
+  # Get First User from the database
+  # User.first
+  before_action :current_user
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user = User.first
   end
 end
