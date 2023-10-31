c283fd26542016a45bef43e5a37437128aed9db1
WHB901 <abdulwhbhussain@gmail.com>
2023-10-31 10:14:19 +0500

    Unneccesary Changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show]
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
 
   # Defines the root path route ("/")
