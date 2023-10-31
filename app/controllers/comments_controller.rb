cd23bb497a58d1411bfc23e4a54365bcbf716de1
WHB901 <abdulwhbhussain@gmail.com>
2023-10-31 14:12:47 +0500

    All Errors are fixed now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,22 @@
 class CommentsController < ApplicationController
+  
+  def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @user.comments.new(post_id: @post.id)
+  end
+
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
     @comment = @user.comments.new(post_id: @post.id, text: comment_params[:text])
 
     respond_to do |format|
-      format.html { redirect_to request.referer } if @comment.save
+      if @comment.save
+        format.html { redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity, locals: { user: @user, post: @post, comment: @comment } }
+      end
     end
   end
 
