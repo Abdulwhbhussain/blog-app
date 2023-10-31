55ac943030308ea0976fc7fd24719137512457ea
WHB901 <abdulwhbhussain@gmail.com>
2023-10-31 12:08:38 +0500

    Add Like and Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(user_id: @user, text: comment_params[:text])
+
+    if @comment.save
+      redirect_to user_post_path(@post.author, @post)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,20 @@
 class LikesController < ApplicationController
+
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.new(user_id: @user.id, like_params)
+
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to user_post_path(@post.author, @post), notice: "Like was successfully created." }
+      end
+    end
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:post_id)
+  end
 end
