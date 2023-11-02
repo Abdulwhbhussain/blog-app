aee232b8a363746000245c06cd2bf04f92c0ae87
WHB901 <abdulwhbhussain@gmail.com>
2023-11-02 10:35:30 +0500

    System and feature test

--- spec/system/system_spec.rb
+++ spec/system/system_spec.rb
@@ -2,6 +2,8 @@
 # To seed the database, Run rails db:seed RAILS_ENV=test
 # To run the test, run rspec spec/system/system_spec.rb
 
+# To run Feature tests, run rspec spec/views/
+
 # System Spec for Blog App
 require 'rails_helper'
 
@@ -10,7 +12,7 @@ RSpec.describe 'Whole System Test', type: :system do
     it 'Shows all users' do
       visit users_path
       sleep(5)
-      expect(page).to have_content('Users')
+      click_on 'View Profile'
     end
   end
 end
