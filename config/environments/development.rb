63d30214880efc5951ee3c9194a9449a4eab4ff2
WHB901 <abdulwhbhussain@gmail.com>
2023-11-08 02:04:01 +0500

    users confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 end
