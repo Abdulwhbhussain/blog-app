ff6aa49b18d3479b5b1ad933015da7ae68569b36
WHB901 <abdulwhbhussain@gmail.com>
2023-11-08 00:25:34 +0500

    Add Devise Configuration and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   # current_user is a method that returns the First User from the database
   # This is a temporary solution to the problem of authentication
   # We will learn how to authenticate users later in the course
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
 end
