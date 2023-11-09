85d8a9733936c25a13d9d8851c20186a124a0517
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-11-09 10:55:27 +0300

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,11 @@
 class Ability
   include CanCan::Ability
+  def initialize(user)
+  user = current_user # guest user (not logged in)
+    if user.admin? # Admin can do anything
+      can :manage, :all
+    else # Normal user can only read
+      can :read, :all
+    end
+  end
 end
