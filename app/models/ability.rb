8e430ce774f048fa97b65d4bd064247dbbdf7359
WHB901 <abdulwhbhussain@gmail.com>
2023-11-09 13:02:01 +0500

    update

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,16 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user = current_user
+
+    if user.admin?
+      can :manage, :all
+    elsif user.user?
+      can :read, :all
+      can :create, :all
+      can :destroy, :all
+    else
+      can :read, :all
+    end
 end
